78ba31a6a307f491db7ae89af7addaa9e7ed7ca9
Oscar Daniel Bueno Alvillar <oscar.bueno@adm.urbe.edu>
2024-09-17 15:48:02 -0400

    modificaciones

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomepageComponent,
+  },
+  {
+    redirectTo: '',
+    pathMatch: 'full',
+    path: '**',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
