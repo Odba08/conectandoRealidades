f3f73caad49591d7499bdd1e0b78f598268a1714
Oscar Daniel Bueno Alvillar <oscar.bueno@adm.urbe.edu>
2024-10-07 15:09:07 -0400

    Modificaciones varias

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
-import { FooterComponent } from './components/footer/footer.component';
+import { SecondNoticeComponent } from './components/second-notice/second-notice.component';
 import { HomepageComponent } from './pages/homepage/homepage.component';
 import { NoticeComponent } from './components/notice/notice.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent,
+    SecondNoticeComponent,
     HomepageComponent,
-    NoticeComponent
+    NoticeComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/second-notice/second-notice.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-second-notice',
+  templateUrl: './second-notice.component.html',
+  styleUrl: './second-notice.component.scss'
+})
+export class SecondNoticeComponent {
+
+}
