7c9d7e53ef387bce9af2cf30a23354da9aa44ae6
Odba08 <miniodba@gmail.com>
2024-11-13 02:58:27 +0100

    Modificaciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router, Event, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'conectando';
+
+  constructor(private router: Router) {}
+
+  ngOnInit(): void {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        window.scrollTo(0, 0); // Desplaza hacia la parte superior
+      }
+    });
+
+}
 }
